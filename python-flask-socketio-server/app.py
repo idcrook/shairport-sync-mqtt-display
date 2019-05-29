ce7815639a9f071ebe1ace07c7d6286bcecb83a5
David Crook <idcrook@users.noreply.github.com>
2019-05-29 16:44:28 -0600

    do some cleanup in app.py and add MQTT connection params
     - extensive documentation added in `app.py`
     - eliminated some code duplication
     - [mqtt]: added username/password
     - [mqtt]: added a param for TLS (for debug only)

--- python-flask-socketio-server/app.py
+++ python-flask-socketio-server/app.py
@@ -36,18 +36,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = WEBSERVER_CONF.get('secret_key', 'secret!')
 socketio = SocketIO(app)
 
-
-def _guessImageMime(magic):
-    """Peeks at a couple bytes in image binary to identify image format."""
-
-    if magic.startswith(b'\xff\xd8'):
-        return 'image/jpeg'
-    elif magic.startswith(b'\x89PNG\r\n\x1a\r'):
-        return 'image/png'
-    else:
-        return "image/jpg"
-
-
 known_track_metadata_types = {
     'artist': 'showArtist',
     'album': 'showAlbum',
@@ -83,44 +71,75 @@ def populateTemplateData(config):
     return templateData
 
 
-# The callback for when MQTT client receives a CONNACK response from the server.
+def _form_subtopic_topic(subtopic):
+    """Return full topic path given subtopic."""
+    topic = TOPIC_ROOT + "/" + subtopic
+    return topic
+
+
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code {}".format(rc))
+    """# For when MQTT client receives a CONNACK response from the server."""
 
-    # adding subscriptions in on_connect() means that if we will still be
-    # subscribed if there lost/reconnect connection to the MQTT server
+    # print("Connected with result code {}".format(rc))
+
+    # adding subscriptions in on_connect() means that they'll be re-subscribed
+    # for lost/re-connection to MQTT server
     for subtopic in ('cover', 'artist', 'album', 'title', 'genre'):
-        sub = TOPIC_ROOT + "/" + subtopic
-        print("Subscribing to topic", sub)
-        # QoS==0 should be fine
-        (result, msg_id) = client.subscribe(sub, 0)
-        # print(msg_id)
+        topic = _form_subtopic_topic(subtopic)
+        print("Subscribing to topic", topic)
+        (result, msg_id) = client.subscribe(topic, 0)  # QoS==0 should be fine
+        print(msg_id)
 
 
-# Callback for when a subscribed-to MQTT message received
+def _guessImageMime(magic):
+    """Peeks at leading bytes in binary object to identify image format."""
+
+    if magic.startswith(b'\xff\xd8'):
+        return 'image/jpeg'
+    elif magic.startswith(b'\x89PNG\r\n\x1a\r'):
+        return 'image/png'
+    else:
+        return "image/jpg"
+
+
+def _send_and_store_playing_metadata(metadata_name, message):
+    """Forms message and sends to browser client using socket.io.
+
+    Also saves a copy of sent message (into SAVED_INFO dict), which is used to
+    resend most recent message in case the browser page is refreshed, another
+    browser client connects, etc.
+
+    Applies a naming convention of prepending string 'playing_' to metadata
+    name in socketio sent message. Of course the same naming convention is used
+    on receiving client.
+    """
+
+    # print("{} update".format(metadata_name))
+    msg = {'data': message.payload.decode('utf8')}
+    emitted_metadata_name = "playing_{}".format(metadata_name)
+    SAVED_INFO[emitted_metadata_name] = msg
+    socketio.emit(emitted_metadata_name, msg)
+
+
 def on_message(client, userdata, message):
-    if message.topic == TOPIC_ROOT + "/" + "artist":
-        print("artist update")
-        msg = {'data': message.payload.decode('utf8')}
-        SAVED_INFO['playing_artist'] = msg
-        socketio.emit('playing_artist', msg)
-    if message.topic == TOPIC_ROOT + "/" + "album":
-        print("album update")
-        msg = {'data': message.payload.decode('utf8')}
-        SAVED_INFO['playing_album'] = msg
-        socketio.emit('playing_album', msg)
-    if message.topic == TOPIC_ROOT + "/" + "genre":
-        print("genre update")
-        msg = {'data': message.payload.decode('utf8')}
-        SAVED_INFO['playing_genre'] = msg
-        socketio.emit('playing_genre', msg)
-    if message.topic == TOPIC_ROOT + "/" + "title":
-        print("title update")
-        msg = {'data': message.payload.decode('utf8')}
-        SAVED_INFO['playing_title'] = msg
-        socketio.emit('playing_title', msg)
-    if message.topic == TOPIC_ROOT + "/" + "cover":
-        print("cover update")
+    """Callback for when a subscribed-to MQTT message is received."""
+
+    # if message.topic != _form_subtopic_topic("cover"):
+    #     print(message.topic, message.payload)
+
+    # Playing track info fields
+    if message.topic == _form_subtopic_topic("artist"):
+        _send_and_store_playing_metadata("artist", message)
+    if message.topic == _form_subtopic_topic("album"):
+        _send_and_store_playing_metadata("album", message)
+    if message.topic == _form_subtopic_topic("genre"):
+        _send_and_store_playing_metadata("genre", message)
+    if message.topic == _form_subtopic_topic("title"):
+        _send_and_store_playing_metadata("title", message)
+
+    # cover art
+    if message.topic == _form_subtopic_topic("cover"):
+        # print("cover update")
         mime_type = _guessImageMime(message.payload)
         b64_image = base64.b64encode(message.payload).decode('utf-8')
         msg = {'data': b64_image, 'mimetype': mime_type}
@@ -145,15 +164,30 @@ if MQTT_CONF.get('use_tls'):
                       tls_version=ssl.PROTOCOL_TLSv1_2,
                       ciphers=None)
 
+        if tls_conf.get('allow_insecure_server_certificate', False):
+            # from docs: Do not use this function in a real system. Setting value
+            # to True means there is no point using encryption.
+            mqttc.tls_insecure_set(True)
+
+if MQTT_CONF.get('username'):
+    username = MQTT_CONF.get('username')
+    print("MQTT username:", username)
+    pw = MQTT_CONF.get('password')
+    if pw:
+        mqttc.username_pw_set(username, password=pw)
+    else:
+        mqttc.username_pw_set(username)
+
 mqtt_host = MQTT_CONF['host']
 mqtt_port = MQTT_CONF['port']
 print("Connecting to broker", mqtt_host, 'port', mqtt_port)
-mqttc.connect(mqtt_host, port=mqtt_port, keepalive=60)
+mqttc.connect_async(mqtt_host, port=mqtt_port)
 mqttc.loop_start()
 
 templateData = populateTemplateData(WEBUI_CONF)
 
 
+# Define Flask server routes
 @app.route("/")
 def main():
     return render_template('main.html',
@@ -163,22 +197,31 @@ def main():
 
 @app.route('/favicon.ico')
 def favicon():
+    """Handle favicon.ico requests."""
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
 
 
-# using this event from browser client to re-send now playing info on a page reload, e.g.
 @socketio.on('myevent')
 def handle_my_custom_event(json):
-    print('received data: ' + str(json))
+    """Re-send now playing info.
+
+    Using this event, typically emitted from browser client, to re-send now
+    playing info on a page reload, e.g.
+
+    """
+    # print('received data: ' + str(json))
+    if json.get('data'):
+        print("myevent:", json['data'])
+
     for key, msg in SAVED_INFO.items():
         # print(key, msg)
         print(key, )
         socketio.emit(key, msg)
 
 
-# launch the Flask (socketio) webserver!
+# launch the Flask (+socketio) webserver!
 if __name__ == "__main__":
     web_host = WEBSERVER_CONF['host']
     web_port = WEBSERVER_CONF['port']
